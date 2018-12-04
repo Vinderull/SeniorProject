9c845a2349510eb5a65f39ef0fc7ea5ed5e2c7ce
philrobb <33300172+philrobb@users.noreply.github.com>
2018-12-04 14:26:49 -0500

    Create ADC1_Wakeup.c

---
+++ Nucleo Proto/ADC1_Wakeup.c
@@ -0,0 +1,21 @@
+/* This ADC1_Wakeup wakes the ADC on the STM32L476 MCU*/
+
+void ADC1_Wakeup (void) {
+  int wait_time;
+  /*Start ADC operations: DEEPPWD = 0: ADC not in deep pwoer mode*/
+  /*DEEPPWD = 1: ADC in deep power mode (reset state) */
+  if ((ADC1 -> CR & ADC_CR_DEEPPWD) == ADC_CR_DEEPPWD){
+    ADC1 -> CR &= ~ADC_CR_DEEPPWD;
+  }
+
+  /* Enable the ADC voltage Regulator */
+  /* must enable before calibration of ADC, use software
+  to wait for regulator startup time */
+  ADC1 -> CR |= ADC_CR_ADVREGEN;
+
+  wait_time = 20 * (80000000 / 100000);
+  while (wait_time != 0){
+  wait_time--;
+  }
+
+}
